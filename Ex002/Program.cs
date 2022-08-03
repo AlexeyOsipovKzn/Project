2ee5f63a951a78b603b17dd0f56e0c13ede238f6
AlexeyOsipovKzn <osipov-leha@inbox.ru>
2022-08-03 19:30:08 +0300

    Username

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Print UserName ");
+string username = Console.ReadLine();
+Console.WriteLine("Hi, ");
+Console.WriteLine(username);
