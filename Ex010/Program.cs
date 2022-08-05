0dad5ca3d282908cde2ac7bc7d47c58f4b40577c
AlexeyOsipovKzn <osipov-leha@inbox.ru>
2022-08-05 21:01:10 +0300

    SQRT

---
+++ Ex010/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Print number");
+int x = int.Parse(Console.ReadLine());// преобразование строки число 
+int sqrt = x*x;
+Console.WriteLine($"Квардрат числа {x} равен {sqrt}");
+sqrt = Convert.ToInt32(Math.Pow); // Вызов библиотеки 
