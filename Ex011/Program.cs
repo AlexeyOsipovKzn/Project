1f9cb84f15db22294ca71331573d0d57eaf7557e
AlexeyOsipovKzn <osipov-leha@inbox.ru>
2022-08-05 21:17:11 +0300

    Является ли квадрат чила

---
+++ Ex011/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Print 2 numbers");
+int x = int.Parse(Console.ReadLine());// преобразование строки число
+int y = int.Parse(Console.ReadLine());// преобразование строки число 
+if(y*y == x)
+{
+    Console.WriteLine($"Число {x} является квадратом чила {y}");
+}
+else 
+{
+    Console.WriteLine($"Число {x} НЕ является квадратом чила {y}");
+}
